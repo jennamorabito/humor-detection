59879f23be825eb3b74cce65fe7baaadf57a90e9
meerwu <meerwu98@gmail.com>
2022-03-30 19:27:26 -0700

    updated file

--- bertembeddings.py
+++ bertembeddings.py
@@ -1,15 +1,15 @@
 import io
 import pandas as pd
-# import torch 
-import numpy as np 
+# import torch
+import numpy as np
 import nltk
 nltk.download('punkt')
 # import re
-# import os 
-# import random 
-from transformers import BertTokenizer, BertModel, BertForSequenceClassification
-# from torch.utils.data import DataLoader, RandomSampler, SequentialSampler 
-from sklearn.metrics import f1_score 
+# import os
+# import random
+from transformers import BertTokenizer, TFBertModel
+# from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+from sklearn.metrics import f1_score
 # from sklearn.model_selection import train_test_split
 from tqdm import tqdm # for nice progress meters
 
@@ -20,31 +20,27 @@ MAX_LENGTH = 100
 
 # tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-def return_id(str1, str2, truncation_strategy, length):
+def return_id(str1, truncation_strategy, length):
     """
-    inputs: 
+    inputs:
     str1, str2: split sentences from the same document
-    truncation_strategry: how the BERT encoder should truncate text. default 'longest_first', other options are . . . 
-    length: either the max sentence length (how many words a sentence can have) or the maximum number of tokens . . . ? 
+    truncation_strategry: how the BERT encoder should truncate text. default 'longest_first', other options are . . .
+    length: either the max sentence length (how many words a sentence can have) or the maximum number of tokens . . . ?
 
     outputs:
     the three outputs from the BERT encoder for one sentence in a list
     """
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', output_hidden_states=True)
-    inputs = tokenizer.encode_plus(str1, str2,
+    inputs = tokenizer.encode_plus(str1,
         add_special_tokens=True,
         max_length=length,
-        truncation=True,
         truncation_strategy=truncation_strategy)
 
-    input_ids =  inputs["input_ids"]
-    input_masks = [1] * len(input_ids)
-    input_segments = inputs["token_type_ids"]
-    padding_length = length - len(input_ids)
+    padding_length = length - len(inputs["input_ids"])
     padding_id = tokenizer.pad_token_id
-    input_ids = input_ids + ([padding_id] * padding_length)
-    input_masks = input_masks + ([0] * padding_length)
-    input_segments = input_segments + ([0] * padding_length)
+    input_ids = np.pad(inputs["input_ids"], (0, padding_length), constant_values=padding_id)
+    input_masks = np.pad(inputs["attention_mask"], (0, padding_length), constant_values=padding_id)
+    input_segments = np.pad(inputs["token_type_ids"], (0, padding_length), constant_values=padding_id)
 
     return [input_ids, input_masks, input_segments]
 
@@ -53,29 +49,33 @@ def compute_input_arrays(df, columns, tokenizer):
     """
     inputs:
     df: dataframe of text data, needs to have sentences to be run through the model + category labels
-    columns: column from df you want to analyze 
+    columns: column from df you want to analyze
     tokenizer: BERT tokenizer to use, default=BertTokenizer.from_pretrained('bert-base-uncased')
-    
+
     outputs: a list of word encodings as np arrays, ready to feed into a neural net
     """
-    
-    # create a list populated with empty lists for 
+
+    # create a list populated with empty lists for
     # the maximum number of sentences you think one of your entries could have,
     # times 3 since each sentence has three outputs after being run through the BERT tokenizer in return_id()
     # plus 3 for the three empty lists that the encoding for the whole sentence will need
-    model_input = []
-    for xx in range((MAX_SENTENCES*3)+3):
-        model_input.append([])
-    
+    model_input = [[]] * ((MAX_SENTENCES+1)*3) # total # of inputs = (# of sentences + doc) * 3 different inputs for bert)
+
+    # padded input ids, masks, and token type ids for texts with less than MAX number of sentences
+    empty_ids, empty_masks, empty_segments = return_id('', 'longest_first', MAX_SENTENCE_LENGTH)
     for _, row in tqdm(df[columns].iterrows()):
         i = 0
-        
+
         # sent
         sentences = nltk.sent_tokenize(row.text) # split each entry into its constituent sentences
         for xx in range(MAX_SENTENCES):
-            # process 1 sent at a time, padded if # of sentences < maximum # of sentences decided earlier
-            s = sentences[xx] if xx<len(sentences) else '' 
-            ids_q, masks_q, segments_q = return_id(s, None, 'longest_first', MAX_SENTENCE_LENGTH) # grab outputs from encoder
+            # process 1 sent at a time
+            if xx < len(sentences):
+                ids_q, masks_q, segments_q = return_id(sentences[xx], 'longest_first', MAX_SENTENCE_LENGTH) # grab outputs from encoder
+            else:
+                ids_q = empty_ids
+                masks_q = empty_masks
+                segments_q = empty_segments
             # put each output for each sentence into sequential spots in the list of empty lists
             model_input[i].append(ids_q)
             i+=1
@@ -83,20 +83,20 @@ def compute_input_arrays(df, columns, tokenizer):
             i+=1
             model_input[i].append(segments_q)
             i+=1
-        
+
         # full row
         # outputs from encoder for whole sentence, put into next three empty lists following the encodings for each sentence
-        ids_q, masks_q, segments_q = return_id(row.text, None, 'longest_first', MAX_LENGTH) 
+        ids_q, masks_q, segments_q = return_id(row.text, 'longest_first', MAX_LENGTH)
         model_input[i].append(ids_q)
         i+=1
         model_input[i].append(masks_q)
         i+=1
         model_input[i].append(segments_q)
-    
+
     # cast each list as an np array, now we have a list of arrays
     for xx in range((MAX_SENTENCES*3)+3):
         model_input[xx] = np.asarray(model_input[xx], dtype=np.int32)
-        
+
     print(model_input[0].shape)
     return model_input
 
