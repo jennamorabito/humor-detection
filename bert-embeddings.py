9a8445246326f604cfa441133d1b46b7b84d95c8
Jenna <jennamorabito25@gmail.com>
2022-04-03 09:59:03 -0700

    tokenizing code that works, we hope

--- bert-embeddings.py
+++ bert-embeddings.py
@@ -1,15 +1,15 @@
 import io
 import pandas as pd
-import torch 
-import numpy as np 
+import torch
+import numpy as np
 import nltk
 nltk.download('punkt')
 # import re
-# import os 
-# import random 
+# import os
+# import random
 from transformers import BertTokenizer, BertModel, BertForSequenceClassification
-from torch.utils.data import DataLoader, RandomSampler, SequentialSampler 
-from sklearn.metrics import f1_score 
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+from sklearn.metrics import f1_score
 from sklearn.model_selection import train_test_split
 from tqdm import tqdm # for nice progress meters
 
@@ -19,7 +19,7 @@ df = pd.read_csv('dataset.csv')
 # split into train and test data
 x_train, x_test, y_train, y_test = train_test_split(df['text'], df['humor'], test_size=0.25, random_state=42, shuffle=True)
 
-# cast back into dataframes 
+# cast back into dataframes
 x_train = x_train.to_frame('text')
 x_test = x_test.to_frame('text')
 y_train = y_train.to_frame('humor')
@@ -30,62 +30,58 @@ MAX_SENTENCES = 5
 MAX_SENTENCE_LENGTH = 20
 MAX_LENGTH = 100
 
-def return_id(str1, str2, truncation_strategy='longest_first', length):
-"""
-inputs: 
-str1, str2: split sentences from the same document
-truncation_strategry: how the BERT encoder should truncate text. default 'longest_first', other options are . . . 
-length: either the max sentence length (how many words a sentence can have) or the maximum number of tokens . . .?  
-"""
+def return_id(str1, truncation_strategy, length):
+    """
+    helper function to get output from the tokenizer into the right form for the model
+    inputs:
+    str1, str2: split sentences from the same document
+    truncation_strategry: how the BERT encoder should truncate text. default 'longest_first', other options are . . .
+    length: either the max sentence length (how many words a sentence can have) or the maximum number of tokens . . . ?
+
+    outputs:
+    the three outputs from the BERT encoder for one sentence in a list
+    """
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', output_hidden_states=True)
-    inputs = tokenizer.encode_plus(str1, str2,
+    inputs = tokenizer.encode_plus(str1,
         add_special_tokens=True,
         max_length=length,
-        truncation_strategy=truncation_strategy)
+        truncation_strategy=truncation_strategy,
+        pad_to_max_length = True)
 
-    input_ids =  inputs["input_ids"]
-    input_masks = [1] * len(input_ids)
-    input_segments = inputs["token_type_ids"]
-    padding_length = length - len(input_ids)
-    padding_id = tokenizer.pad_token_id
-    input_ids = input_ids + ([padding_id] * padding_length)
-    input_masks = input_masks + ([0] * padding_length)
-    input_segments = input_segments + ([0] * padding_length)
-
-    return [input_ids, input_masks, input_segments]
+    return [inputs["input_ids"], inputs["attention_mask"], inputs["token_type_ids"]]
 
 
 def compute_input_arrays(df, columns, tokenizer):
     model_input = []
     for xx in range((MAX_SENTENCES*3)+3):
         model_input.append([])
-    
+
     for _, row in tqdm(df[columns].iterrows()):
         i = 0
-        
+
         # sent
         sentences = nltk.sent_tokenize(row.text)
         for xx in range(MAX_SENTENCES):
             s = sentences[xx] if xx<len(sentences) else ''
-            ids_q, masks_q, segments_q = return_id(s, None, 'longest_first', MAX_SENTENCE_LENGTH)
+            ids_q, masks_q, segments_q = return_id(s, 'longest_first', MAX_SENTENCE_LENGTH)
             model_input[i].append(ids_q)
             i+=1
             model_input[i].append(masks_q)
             i+=1
             model_input[i].append(segments_q)
             i+=1
-        
+
         # full row
-        ids_q, masks_q, segments_q = return_id(row.text, None, 'longest_first', MAX_LENGTH)
+        ids_q, masks_q, segments_q = return_id(row.text, 'longest_first', MAX_LENGTH)
         model_input[i].append(ids_q)
         i+=1
         model_input[i].append(masks_q)
         i+=1
         model_input[i].append(segments_q)
-        
+
     for xx in range((MAX_SENTENCES*3)+3):
         model_input[xx] = np.asarray(model_input[xx], dtype=np.int32)
-        
+
     print(model_input[0].shape)
     return model_input
 
